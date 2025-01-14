ed7f1d38b52335da04c1007ba6e1f7032fb3e305
kobor <kobofare@gmail.com>
2025-01-14 20:35:55 +0800

    export digest class.

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,4 @@ export { fromDidToPublicKey } from './common/codec'
 export { verifyHashBytes, signHashBytes } from './common/signature'
 export { IdentityTemplate } from './wallet/model'
 export * from './yeying/api/web3/web3'
+export {Digest} from "./common/digest"
